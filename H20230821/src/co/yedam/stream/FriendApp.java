b87fc1f8239da870390732c345ba0862cd6dbaca
a <a@DESKTOP-06CCCBG>
2023-08-21 20:47:24 +0900

    과제

--- H20230821/src/co/yedam/stream/FriendApp.java
+++ H20230821/src/co/yedam/stream/FriendApp.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import co.yedam.friend.Friend;
@@ -79,6 +80,7 @@ public class FriendApp {
 				String line =f.getName()+" "+f.getPhone()+" "+sdf.format(f.getBirth());
 				fw.write(line+"\n");
 			}
+			fw.write("\n");
 			fw.flush();
 			fw.close();
 		} catch (IOException e) {
@@ -87,39 +89,31 @@ public class FriendApp {
 	}
 	private void loadFile() {
 		File file= new File("C:/temp/friendList.txt");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Scanner scn = null;
 		try {
-			FileReader fr=new FileReader(file);
-			String line = null;
-			char[] buf=new char[10];
-			while(true) {
-				int val=fr.read(buf);
-				if(val==-1) {
-					break;
-				}
-				for(int i=0;i<val;i++) {
-					line+=buf[i];
-				}
-			}
-			while(true) {
-				String[] lines=line.split("\n");
-				if(lines.equals("")) {
-					break;
-				} else {
-					String[] data =line.split(" ");
-					Date birth=null;
-					try {
-						birth = sdf.parse(data[2]);
-					} catch (ParseException e) {
-						e.printStackTrace();
-					}
-					Friend friend=new Friend(data[0],data[1],birth);
-					friends.add(friend);
-				}
-			}
-		} catch (IOException e) {
+			scn = new Scanner(file);
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		while(true) {
+			String line=null;
+			try{
+				line = scn.nextLine();
+			} catch (NoSuchElementException e) {
+				break;
+			}
+			String[] data=line.split(" ");
+			Friend newFriend = null;
+			try {
+				newFriend = new Friend(data[0], data[1], sdf.parse(data[2]));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			friends.add(newFriend);
+		}
 		
+		}
 		
 	}
-}
+
